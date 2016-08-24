69eb2f468a5e332feab4661b248e934a8959bae3
Reddeyfish <edrichd@uci.edu>
2016-08-23 21:11:33 -0700

    health bars are billboarded and constant size

---
+++ Photon/Assets/Project/UI/WorldSpaceUIPositioning.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldSpaceUIPositioning : MonoBehaviour {
+
+    RectTransform thisTransform;
+
+    [SerializeField]
+    protected Transform target;
+
+    void Start()
+    {
+        thisTransform = (RectTransform)transform;
+        if (transform.root == Camera.main.transform.root)
+        {
+            //this.gameObject.SetActive(false);
+        }
+    }
+
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 worldPos = target.position;
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+        thisTransform.position = screenPos;
+	}
+}
